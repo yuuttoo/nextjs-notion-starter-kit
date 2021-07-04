22485ecd22acd5bd47472705bfbb46c0eb511cc7
Chung Cheng Yu <50949562+yuuttoo@users.noreply.github.com>
2021-07-04 20:04:30 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,8 +19,9 @@ module.exports = {
   // social usernames (optional)
   twitter: '',
   github: 'yuuttoo',
+  medium: 'yuuttoo',
   linkedin: '',
-  medium: 'yuuttoo'
+  
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
